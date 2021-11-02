676b978529ffc53ae1104c1333edf569a8a80ad1
Abhisek Joshi <nofursad@gmail.com>
2021-11-02 21:44:05 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGamer(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
